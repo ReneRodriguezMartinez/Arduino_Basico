1b2b830c2c72d0cdde75a73bb2085710e92c4eed
René Rodríguez Martínez <renerodrim@gmail.com>
2025-02-11 17:14:04 +0100

    Create Encender_un_led.ino

---
+++ Encender_un_led/Encender_un_led.ino
@@ -0,0 +1,18 @@
+/*
+  René Rodríguez Martínez
+  
+  Ejemplo encender un led concetado al pin 3 del arduino
+*/
+const int led= 3; //declaramos el numero del pin donde estara conectado el led
+
+void setup() {
+  //configuramos el pin en modo salida ya que le daremos ordenes de encendido (no recibimos nada)
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  //Encendemos el led en cada vuelta
+  digitalWrite(3,HIGH);
+  
+}
